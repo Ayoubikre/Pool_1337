043f1a4219a913a5829ad1ffc36e023f42b62995
ayoubikr <ayoubikrr@gmail.com>
2024-09-08 05:51:14 +0100

    add

--- Pool/T_pool/C02/ex11/ft_putstr_non_printable.c
+++ Pool/T_pool/C02/ex11/ft_putstr_non_printable.c
@@ -6,10 +6,33 @@
 /*   By: Noctis <Noctis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 01:04:15 by Noctis            #+#    #+#             */
-/*   Updated: 2024/09/03 01:14:05 by Noctis           ###   ########.fr       */
+/*   Updated: 2024/09/06 21:48:49 by Noctis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
 void	ft_putstr_non_printable(char *str)
 {
+	unsigned char	n;
+
+	while (*str)
+	{
+		if (*str > 31 && *str < 127)
+			write(1, str, 1);
+		if (*str < 32 || *str > 126)
+		{
+			n = (unsigned char)*str;
+			write(1, "\\", 1);
+			if ((n / 16) < 10)
+				write(1, &(char){(n / 16) + '0'}, 1);
+			else
+				write(1, &(char){((n / 16) - 10) + 'a'}, 1);
+			if ((n % 16) < 10)
+				write(1, &(char){(n % 16) + '0'}, 1);
+			else
+				write(1, &(char){((n % 16) - 10) + 'a'}, 1);
+		}
+		str++;
+	}
 }
--- Pool/T_pool/C02/ex12/ft_print_memory.c
+++ Pool/T_pool/C02/ex12/ft_print_memory.c
@@ -6,10 +6,139 @@
 /*   By: Noctis <Noctis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 01:07:25 by Noctis            #+#    #+#             */
-/*   Updated: 2024/09/03 01:14:20 by Noctis           ###   ########.fr       */
+/*   Updated: 2024/09/08 05:50:14 by Noctis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void	ft_p1(void *addr)
+{
+	char		buffer[18];
+	uintptr_t	adr2;
+	int			i;
+	int			j;
+	int			k;
+
+	adr2 = (uintptr_t)addr;
+	i = (sizeof(uintptr_t) * 2) - 1;
+	j = 0;
+	while (i >= 0)
+	{
+		k = (adr2 >> (i * 4)) & 0xF;
+		if (k < 10)
+			buffer[j] = '0' + k;
+		else
+			buffer[j] = 'a' + (k - 10);
+		j++;
+		i--;
+	}
+	buffer[j] = '\0';
+	i = 0;
+	while (i < j)
+		write(1, &buffer[i++], 1);
+	write(1, ": ", 2);
+}
+
+void	ft_p2_2(unsigned char c, int count, int i, int flag)
+{
+	int	k;
+
+	if (flag == 1)
+	{
+		while (count < 16)
+		{
+			write(1, " ", 1);
+			count = count + 2;
+		}
+		while (i++ < 16)
+			write(1, "  ", 2);
+	}
+	else
+	{
+		k = c / 16;
+		if (k < 10)
+			write(1, &(char){k + '0'}, 1);
+		else
+			write(1, &(char){(k - 10) + 'a'}, 1);
+		k = c % 16;
+		if (k < 10)
+			write(1, &(char){k + '0'}, 1);
+		else
+			write(1, &(char){(k - 10) + 'a'}, 1);
+	}
+}
+
+void	ft_p2(char *t, void *addr, unsigned int size)
+{
+	unsigned int	i;
+	int				count;
+	uint8_t			*adr2;
+
+	i = 0;
+	count = 0;
+	adr2 = (uint8_t *)addr;
+	while (i < size && i < 16)
+	{
+		if (adr2[i] < 32 || adr2[i] > 126)
+			t[i] = '.';
+		else
+			t[i] = (char)adr2[i];
+		ft_p2_2((unsigned char)adr2[i], 0, 0, 0);
+		count++;
+		if (count % 2 == 0)
+			write(1, " ", 1);
+		i++;
+	}
+	t[i] = '\0';
+	ft_p2_2(' ', count, i, 1);
+}
+
+void	ft_p3(char *addr)
+{
+	while (*addr)
+		write(1, addr++, 1);
+	write(1, "\n", 1);
+}
+
 void	*ft_print_memory(void *addr, unsigned int size)
 {
+	char	t[17];
+
+	if (size == 0)
+		return (addr);
+	ft_p1(addr);
+	ft_p2(t, addr, size);
+	ft_p3(t);
+	if (size > 16)
+	{
+		size -= 16;
+		ft_print_memory(addr + 0x10, size);
+	}
+	if (size < 16)
+		return (addr);
+	return (addr);
 }
+
+// int main(void)
+// {
+// 	char	src[0xFF + 10];
+// 	int		i;
+
+// 	i = 0;
+// 	while (i <= 0xFF)
+// 	{
+// 		src[i] = i;
+// 		i++;
+// 	}
+// 	src[i] = 0;
+// 	i = 0;
+// 	while (i < 30)
+// 	{
+// 		ft_print_memory((void *)src, i);
+// 		i++;
+// 	}
+// 	ft_print_memory((void *)src, 0x101);
+// }
