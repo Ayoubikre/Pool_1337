8654773d9cd1b9129dd44c52c10aec271df1ad1e
ayoubikr <ayoubikrr@gmail.com>
2024-09-22 04:37:30 +0100

    add

--- Pool/T_pool/C12/ex04/ft_list_push_back.c
+++ Pool/T_pool/C12/ex04/ft_list_push_back.c
@@ -1,32 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_list.h                                          :+:      :+:    :+:   */
+/*   ft_list_push_back.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: Noctis <Noctis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/20 17:51:18 by Noctis            #+#    #+#             */
-/*   Updated: 2024/09/22 04:36:48 by Noctis           ###   ########.fr       */
+/*   Created: 2024/09/22 02:27:41 by Noctis            #+#    #+#             */
+/*   Updated: 2024/09/22 04:35:31 by Noctis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_LIST_H
-# define FT_LIST_H
+#include "ft_list.h"
 
-# include <stdlib.h>
-# include <stdio.h>
-
-typedef struct s_list
+void	ft_list_push_back(t_list **begin_list, void *data)
 {
-	struct s_list	*next;
-	void			*data;
-}					t_list;
+	t_list	*n;
+	t_list	*ptr;
 
-t_list	*ft_create_elem(void *data);
-void	ft_list_push_front(t_list **begin_list, void *data);
-int	ft_list_size(t_list *begin_list);
-t_list	*ft_list_last(t_list *begin_list);
-void	ft_list_push_back(t_list **begin_list, void *data);
-
-
-#endif
+	n = ft_create_elem(data);
+	if (n)
+	{
+		if (*begin_list)
+		{
+			ptr = ft_list_last(*begin_list);
+			ptr->next = n;
+		}
+		else
+			*begin_list = n;
+	}
+}
